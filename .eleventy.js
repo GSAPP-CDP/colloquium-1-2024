582eceb955c84a577a4edac9580d62969feb25da
William Martin <william@spatialpixel.com>
2024-07-11 00:20:59 -0400

    Update and remove extraneous dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,4 @@
 const markdownIt = require("markdown-it");
-const markdownItFootnote = require("markdown-it-footnote");
-const markdownImplicitFigures = require("markdown-it-implicit-figures");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 const yaml = require('js-yaml');
@@ -11,42 +9,15 @@ module.exports = function (eleventyConfig) {
   const markdownLib = markdownIt({
     html: true,
     linkify: true,
-  })
-  .use(markdownItFootnote)
-  .use(markdownImplicitFigures, { figcaption: true });
+  });
   
   eleventyConfig.setLibrary("md", markdownLib);
-
+  
   // copy assets from public/ directory to root of site build
   eleventyConfig.addPassthroughCopy({ public: "/" });
   
   // add base plugin to allow deployment to GitHub Pages subfolder
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-  
-  // add filter to normalize links to array of objects with url and text properties
-  eleventyConfig.addFilter("normalizeLinks", function (links) {
-    if (typeof links === "undefined") return;
-    if (!Array.isArray(links)) {
-      throw new Error(
-        "links must be an array of urls or objects with url and text properties"
-      );
-    }
-    return links.map((link) => {
-      if (typeof link === "string") {
-        return { url: link, text: link };
-      }
-      if (typeof link === "object") {
-        if (typeof link.url === "undefined") {
-          throw new Error("link object must have url property");
-        }
-        if (typeof link.text === "undefined") {
-          throw new Error("link object must have text property");
-        }
-        return link;
-      }
-      throw new Error("link must be a string or object");
-    });
-  });
   
   // Copy all HTML files that aren't index.html.
   eleventyConfig.addPassthroughCopy("work/**/!(index).html");
